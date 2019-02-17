0b7f74b033e7d90598d18c52b7be6e278d783dbb
Pankaj sharma <pankaj8.sharma@paytmmall.com>
2019-02-17 18:48:26 +0530

    First Non Repeating Character

--- selfAssignment/src/main/java/string/NoRepeatingCharacterInString.java
+++ selfAssignment/src/main/java/string/NoRepeatingCharacterInString.java
@@ -1,10 +1,30 @@
 package string;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by pankaj on 02,2019
  */
-public class NoRepeatingString {
+public class NoRepeatingCharacterInString {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        char arr[]=sc.nextLine().toCharArray();
+        int countArray[]=new int[256];
+        Arrays.fill(countArray,0);
+        for(int i=0;i<arr.length;i++){
+            countArray[arr[i]]++;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(countArray[arr[i]]==1){
+                System.out.println(arr[i]);
+                break;
+            }
+        }
     }
 }
+
