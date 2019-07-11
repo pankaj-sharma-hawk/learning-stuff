a74b1017eb060f238f7ed39ea445d449a78b332a
Pankaj sharma <pankaj8.sharma@paytmmall.com>
2019-07-11 07:48:32 +0530

    update

--- selfAssignment/src/main/java/rahul/FindPairNumber.java
+++ selfAssignment/src/main/java/rahul/FindPairNumber.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class FindPairNumber {
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int input=scanner.nextInt();
-    int sum=0;
-    int start;
-    sum=start=3;
-    int prev=0;
-    while (sum<input){
-      prev=sum;
-      start*=2;
-      sum=sum+start;
+    while (true) {
+      Scanner scanner = new Scanner(System.in);
+      int input = scanner.nextInt();
+      int sum = 0;
+      int start;
+      sum = start = 3;
+      int prev = 0;
+      while (sum < input) {
+        prev = sum;
+        start *= 2;
+        sum = sum + start;
+      }
+      int diff = input - (prev + 1);
+      System.out.println(start - diff );
     }
-    int diff=input-(prev+1);
-    System.out.println(start-diff+1);
   }
 }
