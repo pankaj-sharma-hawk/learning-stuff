9c793ede62c67f78220be146cc95721af53882c1
Pankaj sharma <pankaj8.sharma@paytmmall.com>
2019-07-14 16:07:57 +0530

    Count bit

--- selfAssignment/src/main/java/amazon/CountNumberOfSetBit.java
+++ selfAssignment/src/main/java/amazon/CountNumberOfSetBit.java
@@ -5,4 +5,21 @@ package amazon;
  */
 public class CountNumberOfSetBit {
 
+  public static void main(String[] args) {
+    System.out.println(countSetbitSimple(8));
+  }
+
+  public static int countSetbitSimple(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+      int temp = i;
+      while (temp > 0) {
+        if ((temp & 1) == 1) {
+          sum++;
+        }
+        temp = temp >> 1;
+      }
+    }
+    return sum;
+  }
 }
